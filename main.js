05a34209a9d344907c1b6bb311fdff10482b1052
L.Carquet <laetitiacarquet@gmail.com>
2017-06-12 13:04:18 +0200

    refactorization

--- main.js
+++ main.js
@@ -4,16 +4,24 @@ $(document).ready(function(){
 		var dropValue = $(this).val()
 
 		//get the html from Ajax call and append it to current html
+		function handleSuccess(response){
+			var response = response;
+			var boxLocalidades = 
+			$('#box_localidades').empty()
+									.append(response);
+		};
+
+
+		function handleError(err){
+			console.log('err')
+		};
+
 		$.ajax({
 			type: "GET",
 			url: 'http://sitelicon.eu/test/ajax_localidades.php/?id='+ dropValue,
-			success: function(response){
-			var response = response;
-			var boxLocalidades = $('#box_localidades').empty().append(response);
-			},
-			error: handleError(err){
-			console.log('err')
-			}
-		})
+			success: handleSuccess,
+			error: handleError
+			})
+	
 	})
 })
