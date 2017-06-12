058f27d16358db577f1e539c0885fe4e807c4653
L.Carquet <laetitiacarquet@gmail.com>
2017-06-12 13:03:40 +0200

    ajax call handling success

--- main.js
+++ main.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
 		//get the value of the drop down menu
 		var dropValue = $(this).val()
 
+		//get the html from Ajax call and append it to current html
+		$.ajax({
+			type: "GET",
+			url: 'http://sitelicon.eu/test/ajax_localidades.php/?id='+ dropValue,
+			success: function(response){
+			var response = response;
+			var boxLocalidades = $('#box_localidades').empty().append(response);
+			},
+			error: handleError(err){
+			console.log('err')
+			}
+		})
+	})
 })
